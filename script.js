56ce43d096b39c6078d007c115e2e52530af321a
belyagg <belyagomel@gmail.com>
2019-11-13 17:08:24 +0300

    task1

---
+++ script.js
@@ -0,0 +1,46 @@
+function isThereACat(animals) {
+    var cat= /cat/gi;
+
+    if  (animals.match(cat)) {
+        console.log("true");
+    } else {
+        console.log("false");
+    }
+}
+
+isThereACat('dog, zebra, Cat, frog');
+isThereACat('dog, zebra, frog');
+isThereACat('dog, zebra, CaT, frog');
+
+
+console.log('-----------------------------------------------------------------------------------');
+
+function isPythagoreanTheorem(a, b, c) {
+    if ((a*a)+(b*b) == (c*c)) {
+        console.log ("true");
+    }else{
+        console.log("false")
+    }
+}
+
+
+isPythagoreanTheorem (6, 8, 10);
+isPythagoreanTheorem (2, 8, 16);
+
+
+console.log('-----------------------------------------------------------------------------------');
+
+
+function gentleSum(a, b) {
+    var Sum = (parseInt(a)+parseInt(b));
+    if (Sum > 0) {
+        console.log('true')
+    }else{
+        console.log('false')
+    }
+}
+
+
+gentleSum ('123sadadas', 5); 
+gentleSum ('hello word', 5);
+gentleSum ('222as', '1asd');
