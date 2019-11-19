1db087c9ab10781161988fc4c0362de43d8f634e
belyagg <belyagomel@gmail.com>
2019-11-19 14:53:47 +0300

    Task2

--- script.js
+++ script.js
@@ -1,46 +1,46 @@
-function isThereACat(animals) {
-    var cat= /cat/gi;
-
-    if  (animals.match(cat)) {
-        console.log("true");
+function deleteObjectValueByKey({name, age}, key) {
+    if (key in deleteObjectValueByKey == true) {
+        delete deleteObjectValueByKey.key;
+        console.log ('Ключ найден и был удален')
     } else {
-        console.log("false");
+        console.log ('Ключ не найдет ');
     }
 }
 
-isThereACat('dog, zebra, Cat, frog');
-isThereACat('dog, zebra, frog');
-isThereACat('dog, zebra, CaT, frog');
+deleteObjectValueByKey ({ name: 'Pete', age: 12}, 'name');
+deleteObjectValueByKey({ name: 'Pete', age: 12}, 'asdasdasdasd');
+deleteObjectValueByKey({ name: 'Pete', age: 12}, 'asdasdasdasd');
 
 
-console.log('-----------------------------------------------------------------------------------');
+let y = 1;
+let num;
 
-function isPythagoreanTheorem(a, b, c) {
-    if ((a*a)+(b*b) == (c*c)) {
-        console.log ("true");
-    }else{
-        console.log("false");
+function printAllEvenNumbers() {
+    do {
+        num = prompt ('Введите число больше 0', 0);
+    } while ( num == 0 || num == NaN || num == null);
+    for (y ; y <= num; y++) {    
+        if (y%2==0) {
+            console.log (y);
+        }
+    }
+  }
+
+printAllEvenNumbers ();
+
+
+let nameUser;
+let isAgeValid;
+
+function checkUser() {
+    nameUser = prompt ('Введите ваше имя');
+    isAgeValid = confirm ('Вам больше 18 ?');
+    if (isAgeValid == true ) {
+        alert ('Добро пожаловать ' + nameUser);
+    } else {
+        alert ( nameUser + ' тебе нет 18 !');
     }
 }
 
+checkUser();
 
-isPythagoreanTheorem (6, 8, 10);
-isPythagoreanTheorem (2, 8, 16);
-
-
-console.log('-----------------------------------------------------------------------------------');
-
-
-function gentleSum(a, b) {
-    var Sum = (parseInt(a)+parseInt(b));
-    if (Sum > 0) {
-        console.log(Sum);
-    }else{
-        console.log('Please enter valid data');
-    }
-}
-
-
-gentleSum ('123sadadas', 5); 
-gentleSum ('hello word', 5);
-gentleSum ('222as', '1asd');
