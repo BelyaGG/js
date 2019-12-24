6efd6c9cef6d45fedb46713c452abfd5a2a90492
belyagg <belyagomel@gmail.com>
2019-12-24 16:26:25 +0300

    no message

--- script1.js
+++ script1.js
@@ -1,19 +1,45 @@
-function loginAndRegUser() {
-  if ( localStorage.getItem('userName') && localStorage.getItem ('password') ){
-    let name = prompt ('Данный пользователь есть,введите логин');
-    let pass = prompt ('Введите пароль');
-    if ( localStorage.getItem('userName') == name && localStorage.getItem('password') == pass ) {
-      document.getElementById('login').style.display = 'block'
-    }else{
-      document.getElementById('error').style.display = 'block'
-    }
-  }else{
-    document.getElementById('reg').style.display = 'block'
-    let name = prompt ('Введите логин',);
-	  localStorage.setItem ('userName' , name);
-  	let pass = prompt ('Введите пароль',);
-	  localStorage.setItem ('password', pass);
-  }
+const HEROES_CONTAINER_ELEMENT = document.querySelector ('.heroes_container');
+
+const getSuperHeroes = () => {
+  return fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
+    .then (response => response.json())
+    .then (heroesArray => {
+      heroesArray.length = 10;
+      return heroesArray;
+    });
 }
 
-loginAndRegUser();
+const createHeroeHTMLElement = ({ name, images: { lg:heroImg } }) => {
+  
+  const heroContainer = document.createElement('div')
+  heroContainer.className = 'hero_container'
+
+  const img = document.createElement('img');
+  img.src = heroImg;
+
+  const heroTitle = document.createElement('div');
+  heroTitle.className = 'hero_title'
+  heroTitle.innerHTML = name;
+
+  heroContainer.appendChild(img);
+  heroContainer.appendChild(heroTitle);
+
+  HEROES_CONTAINER_ELEMENT.appendChild(heroContainer);
+
+}
+
+getSuperHeroes().then(superheroes => {
+  superheroes.map(createHeroeHTMLElement);
+})
+
+function showHeroeHTMLElement() {
+  const heroContainerAll = document.getElementsByClassName('hero_container');
+  setInterval(function() {
+    heroContainerAll.forEach(elem => {
+      elem.setAttribute('show-block', 'show');
+      console.log (elem)
+    });
+  }, 5000);
+}
+
+showHeroeHTMLElement();
