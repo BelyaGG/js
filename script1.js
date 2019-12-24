3b0e1f0ef4ba35c6c65e334a5d725791bf4e28ab
BelyaGG <52006671+BelyaGG@users.noreply.github.com>
2019-12-24 21:09:06 +0300

    Update script1.js

--- script1.js
+++ script1.js
@@ -43,3 +43,69 @@ function showHeroeHTMLElement() {
 }
 
 showHeroeHTMLElement();
+
+
+
+
+
+// const HEROES_CONTAINER_ELEMENT = document.querySelector ('.heroes_container');
+
+// let heroImages;
+// let currentImgIndex = 0;
+
+// let prev = document.getElementById('prev')
+// prev.addEventListener('click', prevSlides)
+// let next = document.getElementById('next')
+// next.addEventListener('click', nextSlides)
+
+// function nextSlides() {
+//   heroImages[currentImgIndex].classList.toggle('visible')
+//   currentImgIndex += 1
+//   if(heroImages[currentImgIndex]){
+//     heroImages[currentImgIndex].classList.toggle("visible");
+//   }else{
+//     currentImgIndex = 0;
+//     heroImages[currentImgIndex].classList.toggle("visible");
+//   }
+// }
+
+// function prevSlides() {
+//   heroImages[currentImgIndex].classList.toggle('visible')
+//   currentImgIndex -= 1
+//   if(heroImages[currentImgIndex]){
+//     heroImages[currentImgIndex].classList.toggle("visible");
+//   }else{
+//     currentImgIndex = heroImages.length - 1;
+//     heroImages[currentImgIndex].classList.toggle("visible");
+//   }
+// }
+
+// const getSuperHeroes = () => {
+//   return fetch('https://cdn.rawgit.com/akabab/superhero-api/0.2.0/api/all.json')
+//     .then (response => response.json())
+//     .then (heroesArray => {
+//       heroesArray.length = 10;
+//       return heroesArray;
+//     });
+// }
+
+// const createHeroeHTMLElement = ({ name, images: { lg:heroImg } }) => {
+//   const img = document.createElement('img');
+//   img.src = heroImg;
+//   HEROES_CONTAINER_ELEMENT.appendChild(img);
+// }
+
+// const getAllHTMLImages = () =>{
+//   return document.querySelectorAll('img')
+// }
+
+
+// getSuperHeroes()
+//   .then(superheroes => {
+//     superheroes.map(createHeroeHTMLElement);
+//   })
+//   .then(getAllHTMLImages)
+//   .then(() =>
+//   heroImages[0]
+//   )
+  
